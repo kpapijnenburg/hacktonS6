fffe71a7d47d28b39169c40c4496e3272b1ad2e8
Ruben Falkenburg <rubenfalkenburg@Rubens-MacBook-Pro.local>
2020-02-13 13:35:37 +0100

    Added Chart

--- src/components/onderzoekers.jsx
+++ src/components/onderzoekers.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
+import DataAdapter from "../util/DataAdapter";
+import ResearchChart from "./onderzoek_chart";
 
 class Onderzoekers extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      
+    };
   }
 
   render() {
-    return <h1>Onderzoekers</h1>;
+    return (
+      <>
+        <ResearchChart />
+      </>
+    )
   }
 }
 
