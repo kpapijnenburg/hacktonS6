488f68e1965f5d6ce0a6df6275940b85069e800e
= <=>
2020-02-13 11:14:56 +0100

    Removed Beheerders link

--- src/components/onderzoekers.jsx
+++ src/components/onderzoekers.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 class Onderzoekers extends Component {
   constructor(props) {
@@ -10,20 +10,29 @@ class Onderzoekers extends Component {
   render() {
     return (
       <section class="col">
-        <button class="btn btn-primary" type="button">Nieuw onderzoek starten</button>
-        <h1>Onderzoekers</h1>
-        <h2>Actieve onderzoeken</h2>
+        <button class="btn btn-primary" type="button">
+          Nieuw onderzoek starten
+        </button>
+        <p className="lead mt-3">Actieve onderzoeken</p>
         <table class="table table-hover">
           <thead class="thead-dark">
             <tr>
-              <th class="col" scope="col">Onderzoeksnaam</th>
-              <th class="col" scope="col">Aantal testpersonen</th>
-              <th class="col" scope="col">Acties</th>
+              <th class="col" scope="col">
+                Onderzoeksnaam
+              </th>
+              <th class="col" scope="col">
+                Aantal testpersonen
+              </th>
+              <th class="col" scope="col">
+                Acties
+              </th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td><h3>Eenzaamheid in Nederland</h3></td>
+              <td>
+                <h3>Eenzaamheid in Nederland</h3>
+              </td>
               <td>129109</td>
               <td>
                 <Link to="/onderzoekers">
@@ -33,7 +42,9 @@ class Onderzoekers extends Component {
               </td>
             </tr>
             <tr>
-              <td><h3>Gameverslaving in Nederland</h3></td>
+              <td>
+                <h3>Gameverslaving in Nederland</h3>
+              </td>
               <td>1387290</td>
               <td>
                 <Link to="/onderzoekers">
@@ -43,7 +54,9 @@ class Onderzoekers extends Component {
               </td>
             </tr>
             <tr>
-              <td><h3>Vermogen van de gemiddelde Nederlander</h3></td>
+              <td>
+                <h3>Vermogen van de gemiddelde Nederlander</h3>
+              </td>
               <td>982139</td>
               <td>
                 <Link to="/onderzoekers">
@@ -55,50 +68,78 @@ class Onderzoekers extends Component {
           </tbody>
         </table>
 
-
         <h2>Inactieve onderzoeken</h2>
         <table class="table table-hover">
           <thead class="thead-dark">
             <tr>
-              <th class="col" scope="col">Onderzoeksnaam</th>
-              <th class="col" scope="col">Aantal testpersonen</th>
-              <th class="col "scope="col">Acties</th>
+              <th class="col" scope="col">
+                Onderzoeksnaam
+              </th>
+              <th class="col" scope="col">
+                Aantal testpersonen
+              </th>
+              <th class="col " scope="col">
+                Acties
+              </th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td><h3>Cyberpesten bij jongeren</h3></td>
+              <td>
+                <h3>Cyberpesten bij jongeren</h3>
+              </td>
               <td>677899</td>
               <td>
                 <Link to="/onderzoekers">
                   <button class="btn btn-info">Data inzien</button>
                 </Link>
-                <button style={{ cursor: "not-allowed" }} class="btn btn-light" disabled>Wijzigen</button>
+                <button
+                  style={{ cursor: "not-allowed" }}
+                  class="btn btn-light"
+                  disabled
+                >
+                  Wijzigen
+                </button>
               </td>
             </tr>
             <tr>
-              <td><h3>Social media veslaving</h3></td>
+              <td>
+                <h3>Social media veslaving</h3>
+              </td>
               <td>3120209</td>
               <td>
                 <Link to="/onderzoekers">
                   <button class="btn btn-info">Data inzien</button>
                 </Link>
-                <button style={{ cursor: "not-allowed" }} class="btn btn-light" disabled>Wijzigen</button>
+                <button
+                  style={{ cursor: "not-allowed" }}
+                  class="btn btn-light"
+                  disabled
+                >
+                  Wijzigen
+                </button>
               </td>
             </tr>
             <tr>
-              <td><h3>Seksuele voorlichting op basisscholen in Nederland</h3></td>
+              <td>
+                <h3>Seksuele voorlichting op basisscholen in Nederland</h3>
+              </td>
               <td>998789</td>
               <td>
                 <Link to="/onderzoekers">
                   <button class="btn btn-info">Data inzien</button>
                 </Link>
-                <button style={{ cursor: "not-allowed" }} class="btn btn-light disabled-button" disabled>Wijzigen</button>
+                <button
+                  style={{ cursor: "not-allowed" }}
+                  class="btn btn-light disabled-button"
+                  disabled
+                >
+                  Wijzigen
+                </button>
               </td>
             </tr>
           </tbody>
         </table>
-
       </section>
     );
   }
